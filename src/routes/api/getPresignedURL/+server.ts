5e4f3c0f0011b14dc84218812d0d72fcfaf58160
Friedrich G. Froebel <froebel.business@gmail.com>
2023-03-21 20:54:37 +0100

    :recycle: Updated aws sdk s3 upload

--- src/routes/api/getPresignedURL/+server.ts
+++ src/routes/api/getPresignedURL/+server.ts
@@ -1,44 +1,51 @@
-import { json } from '@sveltejs/kit'
-import { AWS_BUCKET_REGION, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_BUCKET_NAME } from '$env/static/private'
+import { json } from '@sveltejs/kit';
+import {
+	AWS_BUCKET_REGION,
+	AWS_ACCESS_KEY_ID,
+	AWS_SECRET_ACCESS_KEY,
+	AWS_BUCKET_NAME
+} from '$env/static/private';
 
 import { db } from '$lib/database.server';
-import S3 from "aws-sdk/clients/s3"
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 
 import type { RequestHandler } from './$types';
 
-const s3 = new S3({
-    region: AWS_BUCKET_REGION,
-    accessKeyId: AWS_ACCESS_KEY_ID,
-    secretAccessKey: AWS_SECRET_ACCESS_KEY,
-    signatureVersion: "v4",
+const s3 = new S3Client({
+	region: AWS_BUCKET_REGION,
+	credentials: {
+		accessKeyId: AWS_ACCESS_KEY_ID,
+		secretAccessKey: AWS_SECRET_ACCESS_KEY
+	}
 });
 
-
 export const POST: RequestHandler = async ({ locals }) => {
-    // Create picture reference
-    const user = await db.user.update({
-        where: {
-            username: locals.user.name
-        },
-        data: {
-            progressPictures: {
-                create: [{}]
-            },
-            initPhoto: true,
-        }
-    });
-    // Get id of reference (for usage in filename)
-    // @ts-ignore
-    const { id } = await db.progressPicture.findFirst({
-        where: { user },
-        orderBy: { createdAt: 'desc' }
-    });
+	// Create picture reference
+	const user = await db.user.update({
+		where: {
+			username: locals.user.name
+		},
+		data: {
+			progressPictures: {
+				create: [{}]
+			},
+			initPhoto: true
+		}
+	});
+	// Get id of reference (for usage in filename)
+	// @ts-ignore
+	const { id } = await db.progressPicture.findFirst({
+		where: { user },
+		orderBy: { createdAt: 'desc' }
+	});
 
-    // Make filename = username + datetime
-    const filename = "progress_picture_" + id
+	// Make filename = username + datetime
+	const filename = 'progress_picture_' + id;
 
-    // Make presignedURL
-    const presignedURL = await s3.getSignedUrl('putObject', { Bucket: AWS_BUCKET_NAME, Key: filename, Expires: 60 * 5, })
+	// Make presignedURL
+	const command = new PutObjectCommand({ Bucket: AWS_BUCKET_NAME, Key: filename });
+	const presignedURL = await getSignedUrl(s3, command, { expiresIn: 60 * 5 });
 
-    return json({ url: presignedURL })
-}
+	return json({ url: presignedURL });
+};
