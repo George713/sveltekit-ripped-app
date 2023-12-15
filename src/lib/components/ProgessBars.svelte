3ccefe058cb103d5b6c0f5ac8ea31b5210032bc4
Friedrich G. Froebel <froebel.business@gmail.com>
2023-12-15 23:55:10 +0100

    :bento: Add new main screen components

---
+++ src/lib/components/ProgessBars.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	export let eatenKcal = 0;
+	export let eatenProtein = 0;
+	export let targetKcal = 0;
+	export let targetProtein = 0;
+</script>
+
+<!-- Progress Bars -->
+<!-- <img src="/pizza.svg" alt="pizza.svg" class="bg-black mt-1.5 ml-2" style="float:left;" /> -->
+<div class="rounded bg-gray-200 w-48 h-7 mb-2 shadow-inner">
+	<div
+		class=" rounded bg-yellow-200 h-7 shadow-inner"
+		style="width: {Math.min((eatenKcal / targetKcal) * 100, 100)}%"
+	/>
+</div>
+<!-- <img src="/eggs.svg" alt="eggs.svg" class="bg-black mt-1.5 ml-[0.3rem]" style="float:left;" /> -->
+<div class="rounded bg-gray-200 w-48 h-7 shadow-inner">
+	<div
+		class=" rounded bg-sky-400 h-7 shadow-inner"
+		style="width: {Math.min((eatenProtein / targetProtein) * 100, 100)}%"
+	/>
+</div>
--- src/lib/components/TargetTracker.svelte
+++ src/lib/components/TargetTracker.svelte
@@ -3,33 +3,65 @@
 	export let proteinTarget: number;
 	export let plannedKcal: number;
 	export let plannedProtein: number;
+	export let large: boolean = false;
 </script>
 
-<div class="flex mt-1 ml-2 h-8 grow justify-center items-center">
-	<!-- Calories -->
-	<div class="flex flex-col items-center">
-		<div class="flex">
-			<svg class="h-[10px] w-[10px] mt-[3px] mr-0.5 stroke-yellow-200">
-				<circle cx="5" cy="5" r="2.5" stroke-width="2.5" fill="none" />
-			</svg>
-			<p class="text-[10px] font-light text-neutral-500">Calories</p>
+{#if large}
+	<div class="flex mt-1 ml-2 h-8 grow justify-center items-center">
+		<!-- Calories -->
+		<div class="flex flex-col items-center">
+			<div class="flex">
+				<svg class="h-[10px] w-[10px] mt-[5px] mr-0.5 stroke-yellow-200">
+					<circle cx="5" cy="5" r="3" stroke-width="2.5" fill="none" />
+				</svg>
+				<p class="text-[12px] font-light text-neutral-500">Calories</p>
+			</div>
+			<p class="text-[10px] font-light text-neutral-500">
+				{plannedKcal.toLocaleString('en')} | {calTarget.toLocaleString('en')} kcal
+			</p>
 		</div>
-		<p class="text-[8px] font-light text-neutral-500">
-			{plannedKcal.toLocaleString('en')} | {calTarget.toLocaleString('en')} kcal
-		</p>
-	</div>
-	<!-- Border -->
-	<span class="mx-2 mt-0.5 h-5 w-0.5 bg-neutral-400 rounded-full" />
-	<!-- Protein -->
-	<div class="flex flex-col items-center">
-		<div class="flex">
-			<svg class="h-[10px] w-[10px] mt-[3px] mr-0.5 stroke-cyan-400">
-				<circle cx="5" cy="5" r="2.5" stroke-width="2.5" fill="none" />
-			</svg>
-			<p class="text-[10px] font-light text-neutral-500">Protein</p>
+		<!-- Border -->
+		<span class="mx-2 mt-0.5 h-6 w-0.5 bg-neutral-500 rounded-full" />
+		<!-- Protein -->
+		<div class="flex flex-col items-center">
+			<div class="flex">
+				<svg class="h-[10px] w-[10px] mt-[5px] mr-0.5 stroke-cyan-400">
+					<circle cx="5" cy="5" r="3" stroke-width="2.5" fill="none" />
+				</svg>
+				<p class="text-[12px] font-light text-neutral-500">Protein</p>
+			</div>
+			<p class="text-[10px] font-light text-neutral-500">
+				{plannedProtein} | {proteinTarget} g
+			</p>
 		</div>
-		<p class="text-[8px] font-light text-neutral-500">
-			{plannedProtein} | {proteinTarget} g
-		</p>
 	</div>
-</div>
+{:else}
+	<div class="flex mt-1 ml-2 h-8 grow justify-center items-center">
+		<!-- Calories -->
+		<div class="flex flex-col items-center">
+			<div class="flex">
+				<svg class="h-[10px] w-[10px] mt-[3px] mr-0.5 stroke-yellow-200">
+					<circle cx="5" cy="5" r="2.5" stroke-width="2.5" fill="none" />
+				</svg>
+				<p class="text-[10px] font-light text-neutral-500">Calories</p>
+			</div>
+			<p class="text-[8px] font-light text-neutral-500">
+				{plannedKcal.toLocaleString('en')} | {calTarget.toLocaleString('en')} kcal
+			</p>
+		</div>
+		<!-- Border -->
+		<span class="mx-2 mt-0.5 h-5 w-0.5 bg-neutral-400 rounded-full" />
+		<!-- Protein -->
+		<div class="flex flex-col items-center">
+			<div class="flex">
+				<svg class="h-[10px] w-[10px] mt-[3px] mr-0.5 stroke-cyan-400">
+					<circle cx="5" cy="5" r="2.5" stroke-width="2.5" fill="none" />
+				</svg>
+				<p class="text-[10px] font-light text-neutral-500">Protein</p>
+			</div>
+			<p class="text-[8px] font-light text-neutral-500">
+				{plannedProtein} | {proteinTarget} g
+			</p>
+		</div>
+	</div>
+{/if}
