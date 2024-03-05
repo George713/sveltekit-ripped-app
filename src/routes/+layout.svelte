583ad6dcae60dd4e82a70fd50fa6c2da32f99a57
Friedrich G. Froebel <froebel.business@gmail.com>
2024-03-05 13:49:42 +0100

    :lipstick: Improve layout file

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,24 +4,17 @@
 	import '../app.css';
 </script>
 
-<!-- <svelte:head>
+<svelte:head>
 	<title>SvelteKit Auth</title>
 </svelte:head>
 
-<nav class="flex justify-center bg-slate-400 py-3">
-	{#if !$page.data.user}
+{#if !$page.data.user}
+	<nav class="flex justify-center bg-slate-400 py-3">
 		<a class="bg-cyan-100 px-3 mr-2" href="/">Home</a>
 		<a class="bg-cyan-300 px-3 mx-2" href="/login">Login</a>
 		<a class="bg-cyan-500 px-3 ml-2" href="/register">Register</a>
-	{:else}
-		<a class="bg-cyan-100 px-3 mr-2" href="/">Home</a>
-		<a class="bg-cyan-500 px-3 mx-2" href="/admin">Admin</a>
-
-		<form action="/logout" class="logout" method="POST">
-			<button class="bg-cyan-300 px-3 ml-2" type="submit">Log out</button>
-		</form>
-	{/if}
-</nav> -->
+	</nav>
+{/if}
 
 <main>
 	<slot />
