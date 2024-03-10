bef6880ff34ef8c1a70b1e2bc9177eb188d7e482
Friedrich G. Froebel <froebel.business@gmail.com>
2024-03-10 11:24:20 +0100

    :recycle: Remove unnecessary throw statement

--- src/routes/(protected)/admin/+page.server.ts
+++ src/routes/(protected)/admin/+page.server.ts
@@ -4,6 +4,6 @@ import type { PageServerLoad } from './$types'
 
 export const load: PageServerLoad = async ({ locals }) => {
     if (!locals.user) {
-        throw redirect(302, '/')
+        redirect(302, '/')
     }
 }
