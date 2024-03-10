0b9acaa735c2d1a077a5e19102d0a829916a04d6
Friedrich G. Froebel <froebel.business@gmail.com>
2024-03-10 11:25:30 +0100

    :bento: Update schema

---
+++ prisma/migrations/20240306154044_init/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "progressPicOn" TEXT NOT NULL DEFAULT 'SUN';
---
+++ prisma/migrations/20240306180136_init/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "weeklyPic" BOOLEAN NOT NULL DEFAULT false;
