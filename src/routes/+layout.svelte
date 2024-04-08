f6f5077da0e66ffd0ecca654161290359272a0d9
Friedrich G. Froebel <froebel.business@gmail.com>
2024-04-08 19:16:15 +0200

    :sparkles: Add service worker

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,27 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-
+	import { onMount } from 'svelte';
 	import '../app.css';
+
+	const detectSWUpdate = async () => {
+		const registration = await navigator.serviceWorker.ready;
+
+		registration.addEventListener('updatefound', () => {
+			const newSW = registration.installing;
+			newSW?.addEventListener('statechange', () => {
+				if (newSW.state === 'installed') {
+					if (confirm('New update available! Reload to update?')) {
+						newSW.postMessage({ type: 'SKIP_WAITING' });
+						window.location.reload();
+					}
+				}
+			});
+		});
+	};
+
+	onMount(() => {
+		detectSWUpdate();
+	});
 </script>
 
 <svelte:head>
