56e0b6d48a6cf6b78dce4de2043f82ce76178ea2
Friedrich G. Froebel <froebel.business@gmail.com>
2024-04-08 13:33:39 +0200

    Add vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-// import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-vercel';
 
 
 export default {
 	preprocess: [vitePreprocess()],
 	kit: {
-		// adapter: adapter(),
-		outDir: "public",
+		adapter: adapter(),
+		outDir: "public"
 	}
 };
