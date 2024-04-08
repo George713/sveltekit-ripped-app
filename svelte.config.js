2641d0351ad9b8431c5465637eb2186d2fc1b6d4
Friedrich G. Froebel <froebel.business@gmail.com>
2024-04-08 12:55:57 +0200

    Removed vercel adapter again

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-vercel';
+// import adapter from '@sveltejs/adapter-vercel';
 
 
 export default {
 	preprocess: [vitePreprocess()],
 	kit: {
-		adapter: adapter(),
+		// adapter: adapter(),
 		outDir: "public",
 	}
 };
