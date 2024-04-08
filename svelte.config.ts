9325c27e975c1b1496bda62dc7382db7c9c16456
Friedrich G. Froebel <froebel.business@gmail.com>
2024-04-08 12:53:22 +0200

    Migrate svelte.config js to ts

--- svelte.config.ts
+++ svelte.config.ts
@@ -1,11 +1,12 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import adapter from '@sveltejs/adapter-vercel';
+import type { Config } from '@sveltejs/kit';
 
 
 export default {
 	preprocess: [vitePreprocess()],
 	kit: {
-		// adapter: adapter(),
+		adapter: adapter(),
 		outDir: "public",
 	}
-};
+} satisfies Config;
