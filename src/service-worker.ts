bc061bccd976a3ac4672a8859245d1f804dbd2a2
Friedrich G. Froebel <froebel.business@gmail.com>
2024-04-16 15:19:21 +0200

    :construction: Testing caching of images

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -37,6 +37,9 @@ self.addEventListener('activate', (event) => {
 
 // Listen to fetch events
 self.addEventListener('fetch', (event) => {
+    if (event.request.url.startsWith("https://cdswqmabrloxyfswpggl.supabase.co/storage/v1/object/public/foodItems/foodItem")) {
+        console.log(`request to: ${event.request.url}`)
+    }
     // ignore POST requests etc
     if (event.request.method !== 'GET') return;
 
