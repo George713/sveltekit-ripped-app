2dac0f5dfd31006fc077e6e4574bb419ff0c89af
Friedrich G. Froebel <froebel.business@gmail.com>
2024-04-23 21:25:58 +0200

    :technologist: Add import aliases

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,18 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import adapter from '@sveltejs/adapter-vercel';
 
-
-export default {
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
 	preprocess: [vitePreprocess()],
 	kit: {
 		adapter: adapter(),
-		outDir: "public"
+		outDir: "public",
+		alias: {
+			"$atoms": "./src/lib/components/atoms",
+			"$molecules": "./src/lib/components/molecules",
+			"$organismns": "./src/lib/components/organismns",
+		}
 	}
 };
+
+export default config
