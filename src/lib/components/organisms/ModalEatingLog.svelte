4fe2b79e1c38ab809f76a4f80ecd1382b3efb990
Friedrich G. Froebel <froebel.business@gmail.com>
2024-06-04 15:01:55 +0200

    ♻️ Simplify eating log by using CardArray

--- src/lib/components/organisms/ModalEatingLog.svelte
+++ src/lib/components/organisms/ModalEatingLog.svelte
@@ -1,44 +1,9 @@
 <script lang="ts">
-	import {
-		foodLibrary,
-		plannedItems,
-		visibleView,
-		visibleOverlay,
-		estimatesLog
-	} from '$lib/stores';
+	import { visibleView, visibleOverlay } from '$lib/stores';
 
-	import ItemCard from '$atoms/ItemCard.svelte';
 	import TargetTracker from '$atoms/TargetTracker.svelte';
 	import OverlayAddEstimate from '$overlays/AddEstimate.svelte';
-
-	const eatItem = async (id: number, type: string) => {
-		if (type === 'planned') {
-			plannedItems.update((items) => {
-				const index = items.findIndex((item) => item.id === id);
-				if (index !== -1) {
-					items[index].eaten = true;
-				}
-				return items;
-			});
-		} else if (type === 'estimate') {
-			estimatesLog.update((items) => {
-				const index = items.findIndex((item) => item.id === id);
-				if (index !== -1) {
-					items[index].eaten = true;
-				}
-				return items;
-			});
-		}
-
-		// Updaten eaten flag in db
-		const formData = new FormData();
-		formData.append('id', id.toString());
-		formData.append('type', type);
-		const response = await fetch('?/eatItem', {
-			method: 'POST',
-			body: formData
-		});
-	};
+	import CardArray from '$molecules/CardArray.svelte';
 </script>
 
 <div class="absolute inset-0 h-full w-full bg-black bg-opacity-70">
@@ -68,83 +33,49 @@
 				<TargetTracker />
 			</div>
 			<!--  overflow-y-auto scrollbar-hide -->
-			<div
-				class="scrollbar-hide my-2 flex flex-row flex-wrap justify-center gap-y-1 space-x-1 overflow-y-auto"
-			>
-				{#each $plannedItems as { id, foodId, eaten }}
-					<ItemCard
-						type="bright"
-						{id}
-						{foodId}
-						itemName={foodLibrary.getItemNameByIndex(foodId)}
-						kcal={foodLibrary.getKcalByIndex(foodId)}
-						protein={foodLibrary.getProteinByIndex(foodId)}
-						portionUnit="ptn"
-						portionSize={foodLibrary.getPortionSizeByIndex(foodId)}
-						eatingMenu={true}
-						{eaten}
-						eatItem={() => eatItem(id, 'planned')}
-					/>
-				{/each}
-				{#each $estimatesLog as { id, eaten, kcal, protein }}
-					<ItemCard
-						type="bright"
-						{id}
-						itemName="Estimate"
-						{kcal}
-						{protein}
-						portionUnit="ptn"
-						portionSize={1}
-						eatingMenu={true}
-						{eaten}
-						eatItem={() => eatItem(id, 'estimate')}
-					/>
-				{/each}
-			</div>
+			<CardArray items={['planned', 'estimate']} />
 			<!-- Buttons for Adding Items -->
-			<div class="mb-2">
-				<div class="flex justify-end">
-					<button
-						on:click={() => {
-							visibleOverlay.set('addEstimate');
-						}}
-						class="mx-2 flex h-8 items-center rounded-[4px] bg-neutral-400/70 shadow-[inset_2px_2px_3px_rgba(161,161,161,0.05),inset_-2px_-2px_3px_rgba(0,0,0,0.05)]"
+			<div class="flex justify-end mb-2">
+				<button
+					on:click={() => {
+						visibleOverlay.set('addEstimate');
+					}}
+					class="mx-2 flex h-8 items-center rounded-[4px] bg-neutral-400/70 shadow-[inset_2px_2px_3px_rgba(161,161,161,0.05),inset_-2px_-2px_3px_rgba(0,0,0,0.05)]"
+				>
+					<svg class="ml-1 h-[18px] fill-none stroke-zinc-100 px-1" viewBox="0 0 32 32"
+						><path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="1.5"
+							d="M16,4c-4.963,0-9,4.038-9,9c0,3.186,1.781,5.278,3.212,6.959C11.172,21.085,12,22.059,12,23v5h3 v1h2v-1h3v-5c0-0.941,0.828-1.915,1.788-3.041C23.219,18.278,25,16.186,25,13C25,8.038,20.963,4,16,4z M18,26h-4v-2h4V26z M20.265,18.662c-0.923,1.084-1.805,2.12-2.132,3.338h-4.266c-0.327-1.218-1.209-2.254-2.132-3.338C10.391,17.083,9,15.45,9,13 c0-3.86,3.141-7,7-7s7,3.14,7,7C23,15.45,21.609,17.083,20.265,18.662z M16,7v2c-2.206,0-4,1.794-4,4h-2C10,9.691,12.691,7,16,7z"
+						/></svg
 					>
-						<svg class="ml-1 h-[18px] fill-none stroke-zinc-100 px-1" viewBox="0 0 32 32"
-							><path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								stroke-width="1.5"
-								d="M16,4c-4.963,0-9,4.038-9,9c0,3.186,1.781,5.278,3.212,6.959C11.172,21.085,12,22.059,12,23v5h3 v1h2v-1h3v-5c0-0.941,0.828-1.915,1.788-3.041C23.219,18.278,25,16.186,25,13C25,8.038,20.963,4,16,4z M18,26h-4v-2h4V26z M20.265,18.662c-0.923,1.084-1.805,2.12-2.132,3.338h-4.266c-0.327-1.218-1.209-2.254-2.132-3.338C10.391,17.083,9,15.45,9,13 c0-3.86,3.141-7,7-7s7,3.14,7,7C23,15.45,21.609,17.083,20.265,18.662z M16,7v2c-2.206,0-4,1.794-4,4h-2C10,9.691,12.691,7,16,7z"
-							/></svg
-						>
-						<div
-							class="flex flex-col py-[6px] pr-2 text-[8px] font-bold leading-none tracking-tight text-zinc-100"
-						>
-							<p>Add<br />Estimate</p>
-						</div>
-					</button>
-					<button
-						on:click={() => {
-							visibleView.update('foodLib');
-						}}
-						class="mx-2 flex h-8 items-center rounded-[4px] bg-neutral-400/70 shadow-[inset_2px_2px_3px_rgba(161,161,161,0.05),inset_-2px_-2px_3px_rgba(0,0,0,0.05)]"
+					<div
+						class="flex flex-col py-[6px] pr-2 text-[8px] font-bold leading-none tracking-tight text-zinc-100"
 					>
-						<svg class="ml-1 h-[18px] fill-none stroke-zinc-100 px-1" viewBox="0 0 24 24"
-							><path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								stroke-width="1.5"
-								d="M12 7v10m0-5h5M7 12h2.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-							/></svg
-						>
-						<div
-							class="flex flex-col py-[6px] pr-2 text-[8px] font-bold leading-none tracking-tight text-zinc-100"
-						>
-							<p>Food<br />Library</p>
-						</div>
-					</button>
-				</div>
+						<p>Add<br />Estimate</p>
+					</div>
+				</button>
+				<button
+					on:click={() => {
+						visibleView.update('foodLib');
+					}}
+					class="mx-2 flex h-8 items-center rounded-[4px] bg-neutral-400/70 shadow-[inset_2px_2px_3px_rgba(161,161,161,0.05),inset_-2px_-2px_3px_rgba(0,0,0,0.05)]"
+				>
+					<svg class="ml-1 h-[18px] fill-none stroke-zinc-100 px-1" viewBox="0 0 24 24"
+						><path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="1.5"
+							d="M12 7v10m0-5h5M7 12h2.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+						/></svg
+					>
+					<div
+						class="flex flex-col py-[6px] pr-2 text-[8px] font-bold leading-none tracking-tight text-zinc-100"
+					>
+						<p>Food<br />Library</p>
+					</div>
+				</button>
 			</div>
 		</div>
 		<!-- Minimize Symbol -->
