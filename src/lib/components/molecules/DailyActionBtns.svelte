65fda534198b0f6e50f0bde2e687c328fdbeaf08
Friedrich G. Froebel <froebel.business@gmail.com>
2024-06-06 10:46:10 +0200

    🐛 Fix toggle for weight overlay

--- src/lib/components/molecules/DailyActionBtns.svelte
+++ src/lib/components/molecules/DailyActionBtns.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	import { visibleView } from '$lib/stores';
+	import { visibleOverlay, visibleView } from '$lib/stores';
 
 	export let reset: () => void;
 </script>
@@ -10,7 +10,7 @@
 	<button
 		class="mx-4 h-14 w-14 rounded border border-gray-400/10 bg-gray-300 text-[20px] font-bold text-neutral-600 shadow disabled:bg-gray-200 disabled:text-neutral-400"
 		on:click={() => {
-			visibleView.update('weight');
+			visibleOverlay.set('weight');
 		}}
 		disabled={$page.data.dailyProgress.weighIn || $page.data.dailyProgress.harvest}
 	>
