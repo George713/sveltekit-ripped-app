fcbe1b16013d98eb6cc462901678428ea4cfe034
Friedrich G. Froebel <froebel.business@gmail.com>
2024-06-08 10:37:51 +0200

    🏷️ Add types for supabase auth

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,10 +1,16 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+
+import { SupabaseClient, Session } from '@supabase/supabase-js'
+
 declare global {
 	namespace App {
 		// interface Error {}
 		interface Locals {
+			supabase: SupabaseClient
+			getSession(): Promise<Session | null>
 			user: {
+				id: string,
 				name: string;
 				activeTimeZone: string;
 				pointBalance: number;
