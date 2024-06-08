9929b9de8779a476ad40eda67112de17108c9ceb
Friedrich G. Froebel <froebel.business@gmail.com>
2024-06-08 10:39:36 +0200

    🧱Add supabase auth to hooks

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,18 +1,102 @@
-import type { Handle } from '@sveltejs/kit';
-import { db } from '$lib/database.server';
+import { redirect, type Handle } from '@sveltejs/kit'
+
+import type { Session } from '@supabase/supabase-js'
+import { createServerClient } from '@supabase/ssr'
+import jwt from 'jsonwebtoken'
+import { db } from '$lib/database.server'
+
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
+import { JWT_SECRET } from '$env/static/private';
+import type { SupabaseJwt } from '$lib/types.js'
+
 
 export const handle: Handle = async ({ event, resolve }) => {
-	// get cookies from browser
-	const session = event.cookies.get('session');
+	event.locals.supabase = createServerClient(
+		PUBLIC_SUPABASE_URL,
+		PUBLIC_SUPABASE_ANON_KEY,
+		{
+			cookies: {
+				get: (key) => event.cookies.get(key),
+				set: (key, value, options) => {
+					event.cookies.set(key, value, { ...options, path: '/' })
+				},
+				remove: (key, options) => {
+					event.cookies.delete(key, { ...options, path: '/' })
+				}
+			}
+		}
+	)
 
-	// if no cookies exist
-	if (!session) {
-		return await resolve(event);
+	event.locals.getSession = async (): Promise<Session | null> => {
+		const {
+			data: { session },
+		} = await event.locals.supabase.auth.getSession()
+
+		if (!session) return null
+
+		/**
+		 * Ensures the session is fully validated. See README Security section for details.
+		 * 
+		 * !!! Simply verifying the JWT does not validate the `session.user` object for use. !!!
+		 * See "False Security" in https://github.com/orgs/supabase/discussions/23224
+		 * The safest and easiest way to validate the session is by calling `getUser()`
+		 * and using it's returned data. An alternative, which does not make a network call, 
+		 * is to create a validated session; which we do below. 
+		 */
+		try {
+			const decoded = jwt.verify(session.access_token, JWT_SECRET) as SupabaseJwt
+
+			/**
+			 * Create a validated session.
+			 * 
+			 * Most of these properties are required for functionality or typing.
+			 * Add any data needed for your layouts or pages. In this example,
+			 * the only properties which aren't required are `user.user_metadata.avatar_url & .nickname`,
+			 * otherwise we'd just need to leave `user.user_metadata` as an empty object.
+			 * 
+			 * If possible, avoid using anything from `session.user` to populate these,
+			 * especially unique user data like `id`, an email address, or any other
+			 * user-unique data for queries.
+			 */
+			const validated_session: Session = {
+				access_token: session.access_token,
+				refresh_token: session.refresh_token,
+				expires_at: decoded.exp,
+				expires_in: decoded.exp - Math.round(Date.now() / 1000),
+				token_type: 'bearer',
+				user: {
+					app_metadata: {},
+					aud: 'authenticated',
+					created_at: '',
+					id: decoded.sub,
+					user_metadata: {}
+				}
+			}
+
+			return validated_session
+		} catch (err) {
+			return null
+		}
 	}
+
+	const session = await event.locals.getSession()
+
+	/**
+	 * Only authenticated users can access these paths and their sub-paths.
+	 * 
+	 * If you'd rather do this in your routes, see (authenticated)/app/+page.server.ts
+	 * for an example.
+	 */
+	const auth_protected_paths = new Set(['']) //new Set(['app', 'self'])
+	if (!session && auth_protected_paths.has(event.url.pathname.split('/')[1]))
+		redirect(307, '/login')
+
+	// Get user data
+	const { data, error } = await event.locals.supabase.auth.getUser()
 
 	const user = await db.user.findUnique({
 		where: {
-			userAuthToken: session
+			id: data.user?.id
 		},
 		select: {
 			id: true,
@@ -68,7 +152,6 @@ export const handle: Handle = async ({ event, resolve }) => {
 	});
 
 	if (user) {
-
 		const userWeight = await db.weight.findFirst({
 			where: {
 				userId: user.id
@@ -80,6 +163,7 @@ export const handle: Handle = async ({ event, resolve }) => {
 
 		// User properties
 		event.locals.user = {
+			id: user.id,
 			name: user.username,
 			activeTimeZone: user.activeTimeZone,
 			pointBalance: user.pointBalance,
@@ -108,7 +192,15 @@ export const handle: Handle = async ({ event, resolve }) => {
 		};
 	}
 
-	return await resolve(event);
+	return resolve(event, {
+		/**
+		 * Supabase libraries use the `content-range` and `x-supabase-api-version`
+		 * headers, so we need to tell SvelteKit to pass it through.
+		 */
+		filterSerializedResponseHeaders(name) {
+			return name === 'content-range' || name === 'x-supabase-api-version'
+		},
+	})
 };
 
 const getDateFromXDaysAgo = (days: number) => {
