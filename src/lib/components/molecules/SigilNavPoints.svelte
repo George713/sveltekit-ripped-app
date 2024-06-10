b3ddde9f8f40c636f0db8fc6317a9451300e8a90
Friedrich G. Froebel <froebel.business@gmail.com>
2024-06-10 16:03:04 +0200

    ⚡️Make signOut directly from frontend

--- src/lib/components/molecules/SigilNavPoints.svelte
+++ src/lib/components/molecules/SigilNavPoints.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
+	import { goto, invalidateAll } from '$app/navigation';
 	import { page } from '$app/stores';
 	import SigilBronze from '$atoms/SigilBronze.svelte';
 	import SigilGold from '$atoms/SigilGold.svelte';
 	import SigilWood from '$atoms/SigilWood.svelte';
+
+	export let data;
 </script>
 
 <div class="relative flex h-[calc(50vh)] w-full justify-center bg-slate-100 pt-3">
@@ -11,14 +14,16 @@
 	>
 		G
 	</button>
-	<form action="/logout" class="logout" method="POST">
-		<button
-			class="absolute right-3 flex h-5 w-5 items-center justify-center rounded bg-slate-300 text-[10px] font-bold text-gray-600 shadow-sm"
-			type="submit"
-		>
-			X
-		</button>
-	</form>
+	<button
+		class="absolute right-3 flex h-5 w-5 items-center justify-center rounded bg-slate-300 text-[10px] font-bold text-gray-600 shadow-sm"
+		on:click={() => {
+			data.supabase.auth.signOut();
+			invalidateAll();
+			goto('/', { replaceState: true });
+		}}
+	>
+		X
+	</button>
 	<!-- Points -->
 	<div class="absolute left-[83%] top-[50%]">
 		<span class="text-xs text-gray-500">$</span>
