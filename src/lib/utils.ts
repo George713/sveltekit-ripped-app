3d49882801477a89f9af1899234e548f05de12d3
Friedrich G. Froebel <froebel.business@gmail.com>
2024-06-11 07:07:39 +0200

    🐛Adjust EnterWeight as well

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,8 +5,7 @@ export const selectInput = (event: Event) => {
 }
 
 // For putting the focus automatically on input field
-export const focusElement = (el: HTMLElement) => {
-    Promise.resolve().then(() => {
-        el.focus();
-    })
+export const focusElement = (el: HTMLInputElement) => {
+    el.focus();
+    el.select();
 };
