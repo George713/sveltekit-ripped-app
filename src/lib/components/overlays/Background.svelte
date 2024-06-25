fcdba1f98974c8cb0f7e5e7177bc48df5863106d
Friedrich G. Froebel <froebel.business@gmail.com>
2024-06-25 18:49:51 +0200

    🍱 Make background component fixed

--- src/lib/components/overlays/Background.svelte
+++ src/lib/components/overlays/Background.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div
-	class="absolute z-20 top-0 h-full w-full bg-black bg-opacity-70 backdrop-blur-sm"
+	class="fixed z-20 top-0 left-0 h-full w-full bg-black bg-opacity-70 backdrop-blur-sm"
 	role="button"
 	tabindex={0}
 	on:click|self={() => visibleOverlay.set('none')}
