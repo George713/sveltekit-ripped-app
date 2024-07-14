4fcecb7db7dff12b856cefdcae56568a8ad51003
Friedrich G. Froebel <froebel.business@gmail.com>
2024-07-14 15:08:05 +0200

    🍱 Update to review texts

--- src/lib/components/atoms/ReviewText.svelte
+++ src/lib/components/atoms/ReviewText.svelte
@@ -11,17 +11,17 @@
 					: 'Oh, what is that? Weight gain.';
 	$: textRecommendation =
 		weightDiff < -0.5
-			? 'To keep muscle and primarily lose fat, dropping weight slower is advised.'
+			? 'To keep muscle and primarily lose fat, dropping weight slower is advised. Adjust your calorie target?'
 			: weightDiff < -0.1
 				? 'You are losing weight primarily from fat. Keep going like this!'
 				: weightDiff < 0
 					? "Your weightloss journey is slowing. Monitor if it stays slow like this for 2 consecutive weeks. If so, it's time to change something."
-					: "If you stuck to your diet, it's time to tinker with your intake.";
+					: "If you stuck to your diet, it's time to tinker with your intake. Adjust your calorie target?";
 </script>
 
 <div class="flex flex-col items-center mx-10 my-8">
 	<p class="text-stone-500 text-center font-semibold">{textInterpretation}</p>
 	<p class="text-stone-500 text-center text-sm">
-		{textRecommendation} Adjust your calorie target?
+		{textRecommendation}
 	</p>
 </div>
