0d5d31f7778bdc44d95d6136b1f7988b0729ab3c
Friedrich G. Froebel <froebel.business@gmail.com>
2024-07-19 11:52:27 +0200

    🐛 Fix clickable area for sigil

--- src/lib/components/atoms/sigils/Sigil.svelte
+++ src/lib/components/atoms/sigils/Sigil.svelte
@@ -8,11 +8,11 @@
 </script>
 
 <button
-	class={weeklyReviewOutstanding ? 'animate-pulse absolute' : 'absolute'}
+	class={weeklyReviewOutstanding ? 'absolute animate-pulse' : 'absolute' + ' mt-10'}
 	on:click={() => visibleView.update('review')}
 >
 	<img
-		class="drop-shadow-lg blur-[0.3px] brightness-95 mt-10"
+		class="blur-[0.3px] brightness-95 drop-shadow-lg"
 		width={$page.data.user.currentStatus === 'wood' ? 350 : 330}
 		src="/sigils/{$page.data.user.currentStatus}.webp"
 		alt="Sigil Crest"
