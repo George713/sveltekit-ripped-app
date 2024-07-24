5500a4a839b7b9ae29625e3bf52b0f034167b47c
Friedrich G. Froebel <froebel.business@gmail.com>
2024-07-24 23:08:25 +0200

    🚧 Adding components for new day feature

---
+++ src/lib/components/organisms/NewDay.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import FoodLibraryPlanner from '$molecules/planner/FoodLibraryPlanner.svelte';
+	import Selection from '$molecules/planner/Selection.svelte';
+	import Title from '$atoms/Title.svelte';
+	import MinimizeBtn from '$atoms/MinimizeBtn.svelte';
+	import Background from '$overlays/Background.svelte';
+	import BackgroundPlanel from '$atoms/BackgroundPlanel.svelte';
+</script>
+
+<Background>
+	<BackgroundPlanel>
+		<Title title="New Day" />
+		<Selection title={'Selected'} />
+		<FoodLibraryPlanner newDayPlanning={true} />
+		<MinimizeBtn viewTarget="none" />
+	</BackgroundPlanel>
+</Background>
