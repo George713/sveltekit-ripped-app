2a9085ba21c09fa758176a88920de482286fcf51
Friedrich G. Froebel <froebel.business@gmail.com>
2024-10-29 20:13:15 +0100

    🍱 Add review section to landing page

---
+++ src/lib/components/landingPage/reviews/index.ts
@@ -0,0 +1,2 @@
+export { default as JaneDow } from './JaneDow.png'
+export { default as Stars } from './Stars.svg'
--- src/routes/(wip)/landing-page/+page.svelte
+++ src/routes/(wip)/landing-page/+page.svelte
@@ -8,7 +8,9 @@
 		Mockup
 	} from '$lib/components/landingPage/hero';
 	import { Benefit1Img, Benefit2Img, Benefit3Img } from '$lib/components/landingPage/benefits';
+	import { JaneDow, Stars } from '$lib/components/landingPage/reviews';
 
+	// Benefits
 	interface Benefit {
 		img: string;
 		title: string;
@@ -32,10 +34,35 @@
 		title: 'Holistic Approach',
 		text: 'Through constant feedback and adaptation of your diet, successful progress is guaranteed.'
 	};
+
+	// Reviews
+	interface Review {
+		img: string;
+		text: string;
+		reviewerName: string;
+	}
+
+	const review1: Review = {
+		img: JaneDow,
+		text: '“The Duolingo equivalent of nutrition apps.”',
+		reviewerName: 'Jane Dow'
+	};
+
+	const review2: Review = {
+		img: JaneDow,
+		text: '“This app is already great, but I know it will get even better thanks to the founder.”',
+		reviewerName: 'Jane Dow'
+	};
+
+	const review3: Review = {
+		img: JaneDow,
+		text: '“Other dieting apps have always been a chore for me, but this one is so much fun!”',
+		reviewerName: 'Jane Dow'
+	};
 </script>
 
 <!-- Hero -->
-<div class="flex h-screen w-screen flex-col justify-center bg-stone-900 lg:flex-row">
+<div class="flex h-screen w-full flex-col justify-center bg-stone-900 lg:flex-row">
 	<!-- Mockup for Mobile/Tablet -->
 	<div class="mb-5 mt-2 flex h-[calc(50%)] justify-center lg:hidden">
 		<img class="" src={Mockup} alt="Mockup Mobile" />
@@ -62,7 +89,7 @@
 <!-- Benefits -->
 {#snippet benefit(benefit: Benefit)}
 	<div class="relative flex w-96 flex-col items-center">
-		<img class="absolute mb-3 h-48 object-contain" src={benefit.img} alt="Benefits #1" />
+		<img class="absolute mb-3 h-48 object-contain" src={benefit.img} alt="Benefit" />
 		<div class="mt-40 w-64 text-center">
 			<p class="mb-2 text-xl font-medium">{benefit.title}</p>
 			<p class="text-sm leading-tight">{benefit.text}</p>
@@ -76,9 +103,38 @@
 	<!-- Benefits -->
 	<div class="flex w-full flex-col items-center lg:flex-row lg:justify-evenly">
 		{@render benefit(benefit1)}
-		<div class="mx-auto my-8 h-[3px] w-10 rounded-full bg-stone-600 lg:hidden"></div>
+		<div class="mx-auto my-8 h-0.5 w-10 rounded-full bg-stone-600 lg:hidden"></div>
 		{@render benefit(benefit2)}
-		<div class="mx-auto -mb-3 mt-8 h-[3px] w-10 rounded-full bg-stone-600 lg:hidden"></div>
+		<div class="mx-auto -mb-3 mt-8 h-0.5 w-10 rounded-full bg-stone-600 lg:hidden"></div>
 		{@render benefit(benefit3)}
+	</div>
+</div>
+
+<!-- Reviews / Social Proof -->
+{#snippet review(review: Review)}
+	<div class="flex w-52 flex-col-reverse justify-evenly lg:flex-col lg:items-center">
+		<p class="mt-2 content-center text-sm italic leading-tight lg:mt-0 lg:h-20 lg:text-center">
+			{review.text}
+		</p>
+		<div class="mt-3 flex -translate-x-10 items-center space-x-2 lg:translate-x-0">
+			<img class="w-8 lg:w-10" src={review.img} alt="Review" />
+			<div class="flex flex-col">
+				<p class="text-xs font-medium lg:text-sm">{review.reviewerName}</p>
+				<img class="h-2.5 lg:h-3" src={Stars} alt="Stars" />
+			</div>
+		</div>
+	</div>
+{/snippet}
+
+<div class="flex w-full flex-col bg-stone-900 py-7 text-stone-200">
+	<!-- Section Title -->
+	<h2 class="mb-5 text-center text-xl font-medium">What Alpha Testers Say</h2>
+	<!-- Reviews -->
+	<div class="flex w-full flex-col items-center lg:flex-row lg:justify-evenly">
+		{@render review(review1)}
+		<div class="mx-auto my-8 h-0.5 w-10 rounded-full bg-stone-600 lg:hidden"></div>
+		{@render review(review2)}
+		<div class="mx-auto my-8 h-0.5 w-10 rounded-full bg-stone-600 lg:hidden"></div>
+		{@render review(review3)}
 	</div>
 </div>
