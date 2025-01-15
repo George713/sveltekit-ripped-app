3b9659e7157ec78ebbaeffa861c256665a2862e9
Friedrich G. Froebel <froebel.business@gmail.com>
2025-01-15 13:06:49 +0100

    💄 Fix position of camera field when updating image

--- src/lib/components/atoms/inputs/ItemImage.svelte
+++ src/lib/components/atoms/inputs/ItemImage.svelte
@@ -18,7 +18,7 @@
 <!-- Outer Shape -->
 <div class="relative h-[calc(60%)] w-full">
 	<!-- Image from storage/upload/no image-->
-	{#if foodId && !image}
+	{#if foodId && !image && !showCamera}
 		<img
 			src={`https://cdswqmabrloxyfswpggl.supabase.co/storage/v1/object/public/foodItems/foodItem_${foodId}`}
 			alt="imgUrl"
