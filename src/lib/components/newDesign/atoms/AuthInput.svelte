641450663519db23c990b8d8527e259fd00f7935
Friedrich G. Froebel <froebel.business@gmail.com>
2025-01-28 12:29:39 +0100

    🚧 Working on auth elements

---
+++ src/lib/components/newDesign/atoms/AuthInput.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	interface Props {
+		id: string;
+		placeholder: string;
+		type: string;
+	}
+
+	const { id, placeholder, type }: Props = $props();
+</script>
+
+<input
+	class="h-10 w-3/4 max-w-[270px] appearance-none rounded border border-stone-400 bg-transparent px-3 text-stone-400 placeholder-stone-400 focus:border-2 focus:border-indigo-600 focus:bg-transparent focus:text-stone-400 focus:outline-none"
+	{type}
+	{placeholder}
+	{id}
+	required
+/>
---
+++ src/lib/components/newDesign/molecules/LoginForm.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import AuthInput from '../atoms/AuthInput.svelte';
+	import Button from '../atoms/Button.svelte';
+</script>
+
+<AuthInput id="email" placeholder="Email" type="text" />
+<!-- <AuthInput id="password" placeholder="Password" type="password" /> -->
+<input
+	type="password"
+	id="password"
+	class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+	placeholder="•••••••••"
+	required
+/>
+<Button />
