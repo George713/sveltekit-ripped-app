313dca6b15001299ecdc2d57db079492832610f4
Friedrich G. Froebel <froebel.business@gmail.com>
2025-01-28 12:29:22 +0100

    🍱Add subtle link

---
+++ src/lib/components/newDesign/atoms/SubtleLink.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	interface Props {
+		href: string;
+		text: string;
+	}
+
+	const { href, text }: Props = $props();
+</script>
+
+<a class="absolute right-1 p-2 text-stone-600" {href}>{text}</a>
