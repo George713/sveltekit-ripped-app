b84e5fd60968ee239dc2cbba42c51dbd6edf5466
Friedrich G. Froebel <froebel.business@gmail.com>
2025-01-29 14:52:13 +0100

    ✨Add new register page

---
+++ src/lib/components/newDesign/molecules/RegisterForm.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	import { applyAction, enhance } from '$app/forms';
+
+	import { toastManager } from '$lib/stateManagers.svelte';
+
+	import AuthInput from '../atoms/AuthInput.svelte';
+	import Button from '../atoms/Button.svelte';
+
+	let { form } = $props();
+</script>
+
+<form
+	action="?/register"
+	method="POST"
+	class="flex flex-col items-center space-y-2"
+	use:enhance={() => {
+		return async ({ result }) => {
+			if (result.type === 'failure') {
+				// Show toast with error message
+				toastManager.addToast({
+					type: 'error',
+					message: result.data?.error as string,
+					timeout: 4000
+				});
+			} else {
+				// Otherwise redirect as planned
+				await applyAction(result);
+			}
+		};
+	}}
+>
+	{#if form?.success}
+		<p class="text-center text-2xl font-bold text-stone-200">Success!</p>
+		<p class="mb-10 w-60 text-center text-xl text-stone-200">
+			Check your email inbox for our confirmation email.
+		</p>
+	{:else}
+		<AuthInput id="email" name="email" placeholder="Email" type="email" />
+		<AuthInput id="password" name="password" placeholder="Password" type="password" />
+		<AuthInput
+			id="repeatPassword"
+			name="repeatPassword"
+			placeholder="Repeat password"
+			type="password"
+		/>
+		<Button text="Register" type="submit" />
+	{/if}
+</form>
---
+++ src/routes/new/(auth)/register/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import Logo from '$lib/components/newDesign/atoms/BGLogoBig.svelte';
+	import LogoFooter from '$lib/components/newDesign/atoms/LogoFooter.svelte';
+	import SubtleLink from '$lib/components/newDesign/atoms/SubtleLink.svelte';
+	import RegisterForm from '$lib/components/newDesign/molecules/RegisterForm.svelte';
+
+	let { form } = $props();
+</script>
+
+{#if !form?.success}
+	<SubtleLink href="/login" text="Login" />
+{/if}
+<Logo />
+<div class="mb-1 flex grow flex-col justify-end space-y-20">
+	<RegisterForm {form} />
+	<LogoFooter />
+</div>
