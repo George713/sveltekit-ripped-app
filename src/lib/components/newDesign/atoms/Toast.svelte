59bb207fbca87e8e9f9b5c774cd267f279cee164
Friedrich G. Froebel <froebel.business@gmail.com>
2025-01-29 11:54:18 +0100

    🧑‍💻Add toast functionality

---
+++ src/lib/components/newDesign/atoms/Toast.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { fade } from 'svelte/transition';
+
+	import type { Toast } from '$lib/types';
+
+	interface Props {
+		toast: Toast;
+	}
+
+	let { toast }: Props = $props();
+</script>
+
+{#snippet type(type: string)}
+	{#if type == 'error'}
+		<img src="/icons/Attention.svg" alt="errorIcon" class="absolute" />
+		<p class="mx-10 mb-2 leading-5 font-bold text-rose-700">Error</p>
+	{/if}
+{/snippet}
+
+<div transition:fade class="flex w-full flex-col rounded-lg border border-rose-700 bg-black/60 p-4">
+	{@render type(toast.type)}
+	<p class="mx-10 text-rose-700">
+		{toast.message}
+	</p>
+</div>
---
+++ src/routes/new/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import PotentialToasts from '$lib/components/newDesign/molecules/PotentialToasts.svelte';
+
+	let { children } = $props();
+</script>
+
+<div class="flex h-screen w-screen flex-col bg-stone-900">
+	<PotentialToasts />
+	{@render children?.()}
+</div>
