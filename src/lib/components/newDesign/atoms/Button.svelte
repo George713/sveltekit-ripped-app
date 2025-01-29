041fea4440179691a27c9240be9e8ba699776662
Friedrich G. Froebel <froebel.business@gmail.com>
2025-01-29 11:59:07 +0100

    🧑‍💻 Generalize button component

--- src/lib/components/newDesign/atoms/Button.svelte
+++ src/lib/components/newDesign/atoms/Button.svelte
@@ -1,13 +1,17 @@
 <script lang="ts">
 	interface Props {
 		text: string;
+		type?: 'submit' | 'button' | 'reset' | null | undefined;
+		onclick?: () => void;
 	}
 
-	const { text }: Props = $props();
+	const { text, type = 'submit', onclick }: Props = $props();
 </script>
 
 <button
+	{type}
 	class="h-10 w-3/4 max-w-[270px] rounded-sm bg-indigo-600 px-3 text-[20px] font-semibold text-stone-200"
+	{onclick}
 >
 	{text}
 </button>
