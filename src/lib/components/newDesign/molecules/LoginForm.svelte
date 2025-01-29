d77cb878fbb1f54519c5ef5c61de06c5fda549b5
Friedrich G. Froebel <froebel.business@gmail.com>
2025-01-29 11:59:45 +0100

    ✨Finalize login form

--- src/lib/components/newDesign/molecules/LoginForm.svelte
+++ src/lib/components/newDesign/molecules/LoginForm.svelte
@@ -1,10 +1,33 @@
 <script lang="ts">
+	import { applyAction, enhance } from '$app/forms';
+
+	import { toastManager } from '$lib/stateManagers.svelte';
+
 	import AuthInput from '../atoms/AuthInput.svelte';
 	import Button from '../atoms/Button.svelte';
 </script>
 
-<form action="?/register" method="POST" class="flex flex-col items-center space-y-2">
-	<AuthInput id="email" placeholder="Email" type="text" />
-	<AuthInput id="password" placeholder="Password" type="password" />
-	<Button text="Login" />
+<form
+	action="?/login"
+	method="POST"
+	class="flex flex-col items-center space-y-2"
+	use:enhance={() => {
+		return async ({ result }) => {
+			if (result.type === 'failure') {
+				// Show toast with error message
+				toastManager.addToast({
+					type: 'error',
+					message: result.data?.error as string,
+					timeout: 10000
+				});
+			} else {
+				// Otherwise redirect as planned
+				await applyAction(result);
+			}
+		};
+	}}
+>
+	<AuthInput id="email" name="email" placeholder="Email" type="text" />
+	<AuthInput id="password" name="password" placeholder="Password" type="password" />
+	<Button text="Login" type="submit" />
 </form>
