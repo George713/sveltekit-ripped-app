03d5d482dbeff4e3022c2776673e0d87c20da5aa
Friedrich G. Froebel <froebel.business@gmail.com>
2025-01-29 12:00:51 +0100

    ✨Add new login page

---
+++ src/routes/new/(auth)/login/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import Logo from '$lib/components/newDesign/atoms/BGLogoBig.svelte';
+	import LogoFooter from '$lib/components/newDesign/atoms/LogoFooter.svelte';
+	import SubtleLink from '$lib/components/newDesign/atoms/SubtleLink.svelte';
+	import LoginForm from '$lib/components/newDesign/molecules/LoginForm.svelte';
+</script>
+
+<SubtleLink href="/register" text="Register" />
+<Logo />
+<div class="mb-1 flex grow flex-col justify-end space-y-20">
+	<LoginForm />
+	<LogoFooter />
+</div>
