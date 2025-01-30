f5150fc0e6b78c742972e783edecc70282fa048d
Friedrich G. Froebel <froebel.business@gmail.com>
2025-01-30 20:13:54 +0100

    🍱Add new sigil component

---
+++ src/lib/components/newDesign/atoms/Sigil.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	interface Props {
+		rank: 'tbd' | 'wood' | 'bronze' | 'silver' | 'gold' | 'platinum';
+	}
+
+	let { rank }: Props = $props();
+</script>
+
+<div class="flex w-full justify-center">
+	<img
+		class="w-[320px] {rank === 'tbd' ? `opacity-50 blur-[2px]` : ``}"
+		src="/sigils/{rank}.webp"
+		alt="Sigil Crest"
+	/>
+</div>
---
+++ src/routes/new/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import XPHud from '$lib/components/newDesign/atoms/XPHud.svelte';
+	import Sigil from '$lib/components/newDesign/atoms/Sigil.svelte';
+</script>
+
+<XPHud />
+<Sigil rank={'gold'} />
