d2749346531964601d084edd39b0c4d091e69955
Friedrich G. Froebel <froebel.business@gmail.com>
2025-02-04 14:13:25 +0100

    🍱Add tracking widget

---
+++ src/lib/components/newDesign/molecules/TrackingWidget.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import DigitalCounter from '../atoms/DigitalCounter.svelte';
+	import PowerBar from '../atoms/PowerBar.svelte';
+</script>
+
+<div class="flex flex-col items-center space-y-2" style="transform: translateX(-5%);">
+	<DigitalCounter />
+	<PowerBar />
+</div>
