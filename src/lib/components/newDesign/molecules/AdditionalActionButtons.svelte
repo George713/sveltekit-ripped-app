53057bfa164fd2c86d680f0746bad96802b640ca
Friedrich G. Froebel <froebel.business@gmail.com>
2025-02-04 14:16:17 +0100

    🍱Add additional ActionButtons component

---
+++ src/lib/components/newDesign/molecules/AdditionalActionButtons.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import ActionButton from '../atoms/ActionButton.svelte';
+</script>
+
+<div class="absolute flex items-center space-x-3" style="transform: translate(115%, 134%)">
+	<ActionButton glow={true} size="small" icon="insignia" />
+	<div class="flex flex-col space-y-4">
+		<ActionButton glow={true} size="small" icon="chart" />
+		<ActionButton glow={true} size="small" icon="camera" />
+	</div>
+</div>
