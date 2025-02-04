26126f54fc33db4486bdf1199bf97557f8f918a3
Friedrich G. Froebel <froebel.business@gmail.com>
2025-02-04 14:12:07 +0100

    💄Add Bolt and Eggs icon for powerbars

---
+++ src/lib/components/newDesign/icons/Bolt.svelte
@@ -0,0 +1,96 @@
+<script lang="ts">
+	interface Props {
+		size: 'small' | 'big';
+	}
+	let { size }: Props = $props();
+</script>
+
+{#if size === 'small'}
+	<svg width="19" height="23" viewBox="0 0 19 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<g filter="url(#filter0_d_1619_3924)">
+			<path
+				d="M10.1937 9.42386H14.6363L5.58658 18.5588L7.23199 11.9152H3.94116L6.40928 4.44116H12.1682L11.181 6.93251"
+				stroke="#FBBF24"
+				stroke-width="1.2"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+			/>
+		</g>
+		<defs>
+			<filter
+				id="filter0_d_1619_3924"
+				x="0.341064"
+				y="0.841064"
+				width="17.8953"
+				height="21.3179"
+				filterUnits="userSpaceOnUse"
+				color-interpolation-filters="sRGB"
+			>
+				<feFlood flood-opacity="0" result="BackgroundImageFix" />
+				<feColorMatrix
+					in="SourceAlpha"
+					type="matrix"
+					values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+					result="hardAlpha"
+				/>
+				<feOffset />
+				<feGaussianBlur stdDeviation="1.5" />
+				<feColorMatrix
+					type="matrix"
+					values="0 0 0 0 0.984314 0 0 0 0 0.74902 0 0 0 0 0.141176 0 0 0 1 0"
+				/>
+				<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_1619_3924" />
+				<feBlend
+					mode="normal"
+					in="SourceGraphic"
+					in2="effect1_dropShadow_1619_3924"
+					result="shape"
+				/>
+			</filter>
+		</defs>
+	</svg>
+{:else}
+	<svg width="29" height="35" viewBox="0 0 29 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<g filter="url(#filter0_d_1389_1826)">
+			<path
+				d="M15.958 13.5294H24.4545L7.14685 31L10.2937 18.2941H4L8.72028 4H19.7343L17.8462 8.76471"
+				stroke="#FBBF24"
+				stroke-width="1.68"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+			/>
+		</g>
+		<defs>
+			<filter
+				id="filter0_d_1389_1826"
+				x="0.159912"
+				y="0.159912"
+				width="28.1348"
+				height="34.6802"
+				filterUnits="userSpaceOnUse"
+				color-interpolation-filters="sRGB"
+			>
+				<feFlood flood-opacity="0" result="BackgroundImageFix" />
+				<feColorMatrix
+					in="SourceAlpha"
+					type="matrix"
+					values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+					result="hardAlpha"
+				/>
+				<feOffset />
+				<feGaussianBlur stdDeviation="1.5" />
+				<feColorMatrix
+					type="matrix"
+					values="0 0 0 0 0.984314 0 0 0 0 0.74902 0 0 0 0 0.141176 0 0 0 1 0"
+				/>
+				<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_1389_1826" />
+				<feBlend
+					mode="normal"
+					in="SourceGraphic"
+					in2="effect1_dropShadow_1389_1826"
+					result="shape"
+				/>
+			</filter>
+		</defs>
+	</svg>
+{/if}
---
+++ src/lib/components/newDesign/icons/Eggs.svelte
@@ -0,0 +1,107 @@
+<svg width="21" height="19" viewBox="0 0 21 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<g id="Frame 32">
+		<g id="Combined-Shape" filter="url(#filter0_dddddd_1631_3931)">
+			<path
+				fill-rule="evenodd"
+				clip-rule="evenodd"
+				d="M7.875 3C9.51607 3 10.9678 4.96319 11.8512 7.118C12.5008 6.05404 13.3373 5.26471 14.25 5.26471C16.3211 5.26471 18 9.3295 18 11.4412C18 13.5529 16.3211 15.2647 14.25 15.2647C13.2354 15.2647 12.3149 14.8538 11.6398 14.1864C10.7469 15.2937 9.39185 16 7.875 16C5.18261 16 3 13.7746 3 11.0294C3 10.3306 3.14142 9.46735 3.39667 8.56858L3.51295 8.1816C4.31302 5.65696 5.96585 3 7.875 3ZM7.875 16C10.5674 16 12.75 13.7746 12.75 11.0294C12.75 10.3306 12.6086 9.46735 12.3533 8.56858L12.2371 8.1816C11.437 5.65696 9.78415 3 7.875 3C5.96585 3 4.31302 5.65696 3.51295 8.1816L3.39667 8.56858C3.14142 9.46735 3 10.3306 3 11.0294C3 13.7746 5.18261 16 7.875 16Z"
+				stroke="#DCF9FF"
+				stroke-linecap="round"
+			/>
+		</g>
+	</g>
+	<defs>
+		<filter
+			id="filter0_dddddd_1631_3931"
+			x="0.232"
+			y="0.232"
+			width="20.536"
+			height="18.536"
+			filterUnits="userSpaceOnUse"
+			color-interpolation-filters="sRGB"
+		>
+			<feFlood flood-opacity="0" result="BackgroundImageFix" />
+			<feColorMatrix
+				in="SourceAlpha"
+				type="matrix"
+				values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+				result="hardAlpha"
+			/>
+			<feOffset />
+			<feGaussianBlur stdDeviation="0.027" />
+			<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0.882353 0 0 0 0 1 0 0 0 1 0" />
+			<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_1631_3931" />
+			<feColorMatrix
+				in="SourceAlpha"
+				type="matrix"
+				values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+				result="hardAlpha"
+			/>
+			<feOffset />
+			<feGaussianBlur stdDeviation="0.054" />
+			<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0.882353 0 0 0 0 1 0 0 0 1 0" />
+			<feBlend
+				mode="normal"
+				in2="effect1_dropShadow_1631_3931"
+				result="effect2_dropShadow_1631_3931"
+			/>
+			<feColorMatrix
+				in="SourceAlpha"
+				type="matrix"
+				values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+				result="hardAlpha"
+			/>
+			<feOffset />
+			<feGaussianBlur stdDeviation="0.189" />
+			<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0.882353 0 0 0 0 1 0 0 0 1 0" />
+			<feBlend
+				mode="normal"
+				in2="effect2_dropShadow_1631_3931"
+				result="effect3_dropShadow_1631_3931"
+			/>
+			<feColorMatrix
+				in="SourceAlpha"
+				type="matrix"
+				values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+				result="hardAlpha"
+			/>
+			<feOffset />
+			<feGaussianBlur stdDeviation="0.378" />
+			<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0.882353 0 0 0 0 1 0 0 0 1 0" />
+			<feBlend
+				mode="normal"
+				in2="effect3_dropShadow_1631_3931"
+				result="effect4_dropShadow_1631_3931"
+			/>
+			<feColorMatrix
+				in="SourceAlpha"
+				type="matrix"
+				values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+				result="hardAlpha"
+			/>
+			<feOffset />
+			<feGaussianBlur stdDeviation="0.648" />
+			<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0.882353 0 0 0 0 1 0 0 0 1 0" />
+			<feBlend
+				mode="normal"
+				in2="effect4_dropShadow_1631_3931"
+				result="effect5_dropShadow_1631_3931"
+			/>
+			<feColorMatrix
+				in="SourceAlpha"
+				type="matrix"
+				values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+				result="hardAlpha"
+			/>
+			<feOffset />
+			<feGaussianBlur stdDeviation="1.134" />
+			<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0.882353 0 0 0 0 1 0 0 0 1 0" />
+			<feBlend
+				mode="normal"
+				in2="effect5_dropShadow_1631_3931"
+				result="effect6_dropShadow_1631_3931"
+			/>
+			<feBlend mode="normal" in="SourceGraphic" in2="effect6_dropShadow_1631_3931" result="shape" />
+		</filter>
+	</defs>
+</svg>
