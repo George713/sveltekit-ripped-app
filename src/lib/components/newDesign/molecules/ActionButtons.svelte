f8984db2b44627134ec20b256777b257a5495108
Friedrich G. Froebel <froebel.business@gmail.com>
2025-02-04 22:12:52 +0100

    🍱Connect ActionButton for Weight to Overlay

--- src/lib/components/newDesign/molecules/ActionButtons.svelte
+++ src/lib/components/newDesign/molecules/ActionButtons.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
+	import { visibilityManager } from '$lib/stateManagers.svelte';
+
 	import ActionButton from '../atoms/ActionButton.svelte';
 </script>
 
 <div class="mt-58 flex w-full justify-center space-x-8" style="transform: translateY(-43%)">
-	<ActionButton size="big" icon="scale" glow={true} />
-	<ActionButton size="big" icon="book" glow={true} />
-	<ActionButton size="big" icon="cutlery" glow={true} />
+	<ActionButton
+		size="big"
+		icon="scale"
+		glow={false}
+		onclick={visibilityManager.toggleWeightOverlay}
+	/>
+	<ActionButton size="big" icon="book" glow={false} />
+	<ActionButton size="big" icon="cutlery" glow={false} />
 </div>
