9c8d5d7fd1101bfa6b5a9e0e686ea688cab8d78a
Friedrich G. Froebel <froebel.business@gmail.com>
2025-02-04 22:12:24 +0100

    🍱Add server endpoint for logging weight

---
+++ src/routes/new/+page.server.ts
@@ -0,0 +1,43 @@
+import type { Action, Actions } from "@sveltejs/kit";
+import { fail } from "@sveltejs/kit";
+
+import { prisma } from '$lib/prismaClient.server';
+
+const logWeight: Action = async ({ locals, request }) => {
+    const data = await request.formData();
+    let weight: any = data.get('weight');
+
+    // Check if 'weight' is a number
+    if (isNaN(weight)) {
+        return fail(400);
+    } else {
+        weight = parseFloat(weight);
+    }
+
+    console.log(`Logged weight: ${weight}`);
+
+    // Create weight entry for current user
+    // await prisma.user.update({
+    //     where: {
+    //         id: locals.user.id
+    //     },
+    //     data: {
+    //         weights: {
+    //             create: [{ weight }]
+    //         }
+    //     }
+    // });
+
+    // const collectible = getRandomCollectible()
+
+    return {
+        rewards: {
+            powerups: 1,
+            // collectible: collectible,
+        }
+    }
+};
+
+export const actions: Actions = {
+    logWeight,
+};
