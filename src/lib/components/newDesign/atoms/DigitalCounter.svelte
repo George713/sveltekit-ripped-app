af07f89059d79a32d14af833523a36ad7d55a06e
Friedrich G. Froebel <froebel.business@gmail.com>
2025-02-04 14:13:10 +0100

    🍱Add digital counter component

---
+++ src/lib/components/newDesign/atoms/DigitalCounter.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	let kcal = 1234;
+	let kcalTarget = 2300;
+	let protein = 85;
+	let proteinTarget = 160;
+</script>
+
+<div class="ml-5 flex items-center">
+	<!-- Calories -->
+	<div class="flex flex-col items-center">
+		<!-- Icon with Label -->
+		<div class="flex items-center space-x-1.5">
+			<!-- Yellow Ring -->
+			<div class="mb-0.5 size-0.5 rounded-full ring-2 ring-amber-300"></div>
+			<!-- Label -->
+			<p class="text-xs text-stone-500">Calories</p>
+		</div>
+		<!-- Digital Counter -->
+		<p class="text-[10px] text-stone-200">
+			{kcal.toLocaleString('en-US')} | {kcalTarget.toLocaleString('en-US')} kcal
+		</p>
+	</div>
+	<!-- Separator -->
+	<div class="mx-2 h-6 w-0.5 rounded-full bg-stone-700"></div>
+	<!-- Protein -->
+	<div class="flex flex-col items-center">
+		<!-- Icon with Label -->
+		<div class="flex items-center space-x-1.5">
+			<!-- Yellow Ring -->
+			<div class="mb-0.5 size-0.5 rounded-full ring-2 ring-cyan-300"></div>
+			<!-- Label -->
+			<p class="text-xs text-stone-500">Protein</p>
+		</div>
+		<!-- Digital Counter -->
+		<p class="text-[10px] text-stone-200">
+			{protein} | {proteinTarget} g
+		</p>
+	</div>
+</div>
