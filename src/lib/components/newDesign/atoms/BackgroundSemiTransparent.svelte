d9eb323dffdb5a6933ee1932690ac7d2507add9a
Friedrich G. Froebel <froebel.business@gmail.com>
2025-02-04 22:11:17 +0100

    🍱Add semi transparent background overlay

---
+++ src/lib/components/newDesign/atoms/BackgroundSemiTransparent.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	interface Props {
+		onclick?: (e: Event) => void;
+		classAddons?: string;
+		children: Snippet;
+	}
+	let { onclick, classAddons = '', children }: Props = $props();
+</script>
+
+<button
+	class="fixed top-0 left-0 h-full w-full bg-black/40 backdrop-blur-xs {classAddons}"
+	{onclick}
+>
+	{@render children?.()}
+</button>
