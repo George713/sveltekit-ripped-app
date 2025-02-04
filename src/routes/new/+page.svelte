41c85e423ac2c75c125b18b6e1bf690deb94af9d
Friedrich G. Froebel <froebel.business@gmail.com>
2025-02-04 14:17:14 +0100

    ✨ Add ActionPanel to main screen

--- src/routes/new/+page.svelte
+++ src/routes/new/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import XPHud from '$lib/components/newDesign/atoms/XPHud.svelte';
 	import Sigil from '$lib/components/newDesign/atoms/Sigil.svelte';
+	import ActionPanel from '$lib/components/newDesign/organisms/ActionPanel.svelte';
 </script>
 
 <XPHud />
 <Sigil rank={'gold'} />
+<ActionPanel />
