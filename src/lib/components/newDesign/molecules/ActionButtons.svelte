4966711e158ee65223d9bc6ea300ed073608b454
Friedrich G. Froebel <froebel.business@gmail.com>
2025-02-04 14:16:02 +0100

    🍱Add main ActionButtons component

---
+++ src/lib/components/newDesign/molecules/ActionButtons.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import ActionButton from '../atoms/ActionButton.svelte';
+</script>
+
+<div class="mt-58 flex w-full justify-center space-x-8" style="transform: translateY(-43%)">
+	<ActionButton size="big" icon="scale" glow={true} />
+	<ActionButton size="big" icon="book" glow={true} />
+	<ActionButton size="big" icon="cutlery" glow={true} />
+</div>
