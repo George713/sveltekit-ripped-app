d00726f99cdd3942bd65bbe45eaf59a46a8b06d4
Friedrich G. Froebel <froebel.business@gmail.com>
2025-02-04 22:10:40 +0100

    💄Turn glow off for addi ActionButtons

--- src/lib/components/newDesign/molecules/AdditionalActionButtons.svelte
+++ src/lib/components/newDesign/molecules/AdditionalActionButtons.svelte
@@ -3,9 +3,9 @@
 </script>
 
 <div class="absolute flex items-center space-x-3" style="transform: translate(115%, 134%)">
-	<ActionButton glow={true} size="small" icon="insignia" />
+	<ActionButton glow={false} size="small" icon="insignia" />
 	<div class="flex flex-col space-y-4">
-		<ActionButton glow={true} size="small" icon="chart" />
-		<ActionButton glow={true} size="small" icon="camera" />
+		<ActionButton glow={false} size="small" icon="chart" />
+		<ActionButton glow={false} size="small" icon="camera" />
 	</div>
 </div>
