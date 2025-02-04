6cecaa19c89889e4ec57312fec887c08d71cec1b
Friedrich G. Froebel <froebel.business@gmail.com>
2025-02-04 14:16:49 +0100

    🍱Compose action panel for main screen

---
+++ src/lib/components/newDesign/organisms/ActionPanel.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import PowerUps from '../atoms/PowerUps.svelte';
+	import ConquerorButton from '../atoms/ConquerorButton.svelte';
+	import PowerLines from '../atoms/powerlines/PowerLines.svelte';
+	import ActionButtons from '../molecules/ActionButtons.svelte';
+	import AdditionalActionButtons from '../molecules/AdditionalActionButtons.svelte';
+	import TrackingWidget from '../molecules/TrackingWidget.svelte';
+</script>
+
+<div class="mt-auto flex flex-col items-center">
+	<TrackingWidget />
+	<PowerLines />
+	<PowerUps />
+	<ConquerorButton />
+	<AdditionalActionButtons />
+	<ActionButtons />
+</div>
