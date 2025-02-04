71c57023e9d91f40445df0f3d0647579b36cb469
Friedrich G. Froebel <froebel.business@gmail.com>
2025-02-04 23:36:52 +0100

    💄Connect sigil rank with user data

--- src/routes/new/(protected)/+page.svelte
+++ src/routes/new/(protected)/+page.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
+	import { page } from '$app/state';
+
+	import { visibilityManager } from '$lib/stateManagers.svelte';
+
 	import XPHud from '$lib/components/newDesign/atoms/XPHud.svelte';
 	import Sigil from '$lib/components/newDesign/atoms/Sigil.svelte';
 	import ActionPanel from '$lib/components/newDesign/organisms/ActionPanel.svelte';
 	import WeightOverlay from '$lib/components/newDesign/organisms/WeightOverlay.svelte';
-
-	import { visibilityManager } from '$lib/stateManagers.svelte';
 </script>
 
 <XPHud />
-<Sigil rank={'gold'} />
+<Sigil rank={page.data.user.currentStatus} />
 <ActionPanel />
 {#if visibilityManager.weightOverlay}
 	<WeightOverlay />
