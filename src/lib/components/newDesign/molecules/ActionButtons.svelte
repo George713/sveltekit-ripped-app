16ee52b1d301fe6e16764da30d6f05664bc371ae
Friedrich G. Froebel <froebel.business@gmail.com>
2025-02-05 18:50:03 +0100

    🍱Connect actionbuttons with new properties

--- src/lib/components/newDesign/molecules/ActionButtons.svelte
+++ src/lib/components/newDesign/molecules/ActionButtons.svelte
@@ -10,8 +10,14 @@
 		size="big"
 		icon="scale"
 		glow={page.data.dailyProgress.weighIn}
+		disabled={page.data.dailyProgress.weighIn}
 		onclick={visibilityManager.toggleWeightOverlay}
 	/>
-	<ActionButton size="big" icon="book" glow={page.data.dailyProgress.planned} />
-	<ActionButton size="big" icon="cutlery" glow={false} />
+	<ActionButton
+		size="big"
+		icon="book"
+		glow={page.data.dailyProgress.planned}
+		disabled={page.data.dailyProgress.planned}
+	/>
+	<ActionButton size="big" icon="cutlery" glow={false} disabled={page.data.dailyProgress.harvest} />
 </div>
--- src/lib/components/newDesign/molecules/AdditionalActionButtons.svelte
+++ src/lib/components/newDesign/molecules/AdditionalActionButtons.svelte
@@ -1,11 +1,33 @@
 <script lang="ts">
+	import { page } from '$app/state';
 	import ActionButton from '../atoms/ActionButton.svelte';
 </script>
 
 <div class="absolute flex items-center space-x-3" style="transform: translate(115%, 134%)">
-	<ActionButton glow={false} size="small" icon="insignia" />
+	<!-- Bodyfat -->
+	<ActionButton
+		glow={false}
+		size="small"
+		icon="insignia"
+		disabled={page.data.user.enterBodyfatToday}
+		hidden={!page.data.user.enterBodyfatToday}
+	/>
 	<div class="flex flex-col space-y-4">
-		<ActionButton glow={false} size="small" icon="chart" />
-		<ActionButton glow={false} size="small" icon="camera" />
+		<!-- Review -->
+		<ActionButton
+			glow={false}
+			size="small"
+			icon="chart"
+			disabled={page.data.dailyProgress.weeklyReview}
+			hidden={!page.data.user.reviewToday}
+		/>
+		<!-- Photo -->
+		<ActionButton
+			glow={false}
+			size="small"
+			icon="camera"
+			disabled={page.data.dailyProgress.weeklyPic}
+			hidden={!page.data.user.progressPicToday}
+		/>
 	</div>
 </div>
