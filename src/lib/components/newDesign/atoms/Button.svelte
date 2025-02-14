63b73269e5d978f653b6daab614f6596e704cf11
Friedrich G. Froebel <froebel.business@gmail.com>
2025-02-14 13:56:21 +0100

    🍱 Update positioning on planner page and add button style

--- src/lib/components/newDesign/atoms/Button.svelte
+++ src/lib/components/newDesign/atoms/Button.svelte
@@ -3,14 +3,20 @@
 		text: string;
 		type?: 'submit' | 'button' | 'reset' | null | undefined;
 		onclick?: () => void;
+		variant?: 'primary' | 'secondary';
 	}
 
-	const { text, type = 'submit', onclick }: Props = $props();
+	const { text, type = 'submit', onclick, variant = 'primary' }: Props = $props();
 </script>
 
 <button
 	{type}
-	class="h-10 w-3/4 max-w-[270px] rounded-sm bg-indigo-600 px-3 text-xl font-semibold text-stone-200"
+	class={{
+		'rounded-sm px-3': true,
+		'h-10 w-3/4 max-w-[270px] bg-indigo-600 text-xl font-semibold text-stone-200':
+			variant === 'primary',
+		'border border-stone-500 py-2.5 text-xs text-stone-500': variant === 'secondary'
+	}}
 	{onclick}
 >
 	{text}
--- src/routes/(protected)/planner/+page.svelte
+++ src/routes/(protected)/planner/+page.svelte
@@ -4,11 +4,19 @@
 	import Minimizer from '$lib/components/newDesign/atoms/Minimizer.svelte';
 	import FoodLibrary from '$lib/components/newDesign/organisms/FoodLibrary.svelte';
 	import FoodSelection from '$lib/components/newDesign/organisms/FoodSelection.svelte';
+
+	const endPlanning = async () => {
+		// TODO: Implement end planning
+	};
 </script>
 
-<div class="flex h-screen w-screen flex-col-reverse items-center gap-y-2 p-1">
+<div class="mb-1 flex h-screen w-screen flex-col-reverse items-center p-1">
 	<Minimizer onclick={() => goto('/')} />
-	<Button text="End Planning" />
+	<div class="h-8"></div>
+	<div class="flex w-full justify-end p-2">
+		<Button text="End Planning" variant="secondary" onclick={endPlanning} />
+	</div>
 	<FoodLibrary />
+	<div class="h-8"></div>
 	<FoodSelection />
 </div>
