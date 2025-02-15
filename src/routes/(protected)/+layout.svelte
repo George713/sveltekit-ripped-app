1e5d46ab40c7edd8decd181df37ae7f89e803f0c
Friedrich G. Froebel <froebel.business@gmail.com>
2025-02-16 00:27:29 +0100

    ⚗️Testing dynamic viewport sizing

--- src/routes/(protected)/+layout.svelte
+++ src/routes/(protected)/+layout.svelte
@@ -36,7 +36,7 @@
 	});
 </script>
 
-<div class="flex h-screen w-screen flex-col bg-linear-to-br/oklch from-stone-900 to-stone-800">
+<div class="flex h-dvh w-dvw flex-col bg-linear-to-br/oklch from-stone-900 to-stone-800">
 	{#if visibilityManager.spinnerOverlay}
 		<SpinnerOverlay />
 	{/if}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,6 @@
 	let { children } = $props();
 </script>
 
-<div class="flex h-screen w-screen flex-col bg-linear-to-br/oklch from-stone-900 to-stone-800">
+<div class="flex h-dvh w-dvw flex-col bg-linear-to-br/oklch from-stone-900 to-stone-800">
 	{@render children?.()}
 </div>
