1dd90abaceb096eb5aeeda71712a729e528f46a4
Friedrich G. Froebel <froebel.business@gmail.com>
2025-02-16 00:06:49 +0100

    Revert last change

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,6 @@ const config = {
 	preprocess: [vitePreprocess()],
 	kit: {
 		adapter: adapter(),
-		exclude: ['src/routes/old'],
 		outDir: "public",
 		alias: {
 			"$atoms": "./src/lib/components/atoms",
