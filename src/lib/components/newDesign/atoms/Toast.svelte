8a308e6a1cdaa78b649dc078b9a51a987e3761c0
Friedrich G. Froebel <froebel.business@gmail.com>
2025-02-15 23:46:01 +0100

    🍱Update toast component for attention case

--- src/lib/components/newDesign/atoms/Toast.svelte
+++ src/lib/components/newDesign/atoms/Toast.svelte
@@ -2,6 +2,7 @@
 	import { fade } from 'svelte/transition';
 
 	import type { Toast } from '$lib/types';
+	import Attention from '../icons/Attention.svelte';
 
 	interface Props {
 		toast: Toast;
@@ -10,16 +11,31 @@
 	let { toast }: Props = $props();
 </script>
 
-{#snippet type(type: string)}
-	{#if type == 'error'}
-		<img src="/icons/Attention.svg" alt="errorIcon" class="absolute" />
-		<p class="mx-10 mb-2 leading-5 font-bold text-rose-700">Error</p>
-	{/if}
-{/snippet}
-
-<div transition:fade class="flex w-full flex-col rounded-lg border border-rose-700 bg-black/60 p-4">
-	{@render type(toast.type)}
-	<p class="mx-10 text-rose-700">
+<div
+	transition:fade
+	class={{
+		'flex w-full flex-col rounded-lg border bg-black/60 p-4 backdrop-blur-xs': true,
+		'border-rose-700': toast.type == 'error',
+		'border-amber-500': toast.type == 'attention'
+	}}
+>
+	<Attention color={toast.type === 'error' ? 'rose' : 'amber'} classAddons="absolute" />
+	<p
+		class={{
+			'mx-10 mb-2 leading-5 font-bold': true,
+			'text-rose-700': toast.type == 'error',
+			'text-amber-500': toast.type == 'attention'
+		}}
+	>
+		{toast.type.charAt(0).toUpperCase() + toast.type.slice(1)}
+	</p>
+	<p
+		class={{
+			'mx-10 ': true,
+			'text-rose-700': toast.type == 'error',
+			'text-amber-500': toast.type == 'attention'
+		}}
+	>
 		{toast.message}
 	</p>
 </div>
