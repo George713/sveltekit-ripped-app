936c60893546d7b005022bb09b34335ffbd6d930
Friedrich G. Froebel <froebel.business@gmail.com>
2025-02-16 13:46:10 +0100

    🍱Add photoFrame and itemName input

---
+++ src/lib/components/newDesign/atoms/inputs/ItemName.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+</script>
+
+<input
+	placeholder="Item Name..."
+	name="itemName"
+	type="text"
+	spellcheck="false"
+	required
+	class="text-center text-xl text-stone-200 placeholder:text-stone-600"
+/>
--- src/routes/(protected)/newItem/+page.svelte
+++ src/routes/(protected)/newItem/+page.svelte
@@ -1,14 +1,18 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
+	import ItemName from '$lib/components/newDesign/atoms/inputs/ItemName.svelte';
 	import Minimizer from '$lib/components/newDesign/atoms/Minimizer.svelte';
+	import PhotoFrame from '$lib/components/newDesign/atoms/PhotoFrame.svelte';
 </script>
 
 <!-- Minimizer horizontal -->
-<div class="flex h-screen w-screen flex-col">
-	<Minimizer onclick={() => goto('/planner')} direction="left" />
+<div class="flex h-screen w-screen flex-col items-center">
+	<div class="justify-left flex w-full">
+		<Minimizer onclick={() => goto('/planner')} direction="left" />
+	</div>
+	<PhotoFrame />
+	<ItemName />
+	<!-- Add & Record Button (grow) -->
+	<!-- kcal | protein sums -->
+	<!-- Create Item -->
 </div>
-<!-- Photo Frame -->
-<!-- Item Name -->
-<!-- Add & Record Button (grow) -->
-<!-- kcal | protein sums -->
-<!-- Create Item -->
