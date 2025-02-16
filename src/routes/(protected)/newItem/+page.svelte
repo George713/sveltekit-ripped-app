24e3cfcb1634bd171100bf4856c3d6aedeb311e9
Friedrich G. Froebel <froebel.business@gmail.com>
2025-02-16 18:03:20 +0100

    💄Finish default view of newItem

--- src/routes/(protected)/newItem/+page.svelte
+++ src/routes/(protected)/newItem/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
+	import Button from '$lib/components/newDesign/atoms/Button.svelte';
 	import ItemName from '$lib/components/newDesign/atoms/inputs/ItemName.svelte';
 	import Minimizer from '$lib/components/newDesign/atoms/Minimizer.svelte';
 	import NutrientSum from '$lib/components/newDesign/atoms/NutrientSum.svelte';
@@ -7,14 +8,19 @@
 	import IngredientInput from '$lib/components/newDesign/molecules/IngredientInput.svelte';
 </script>
 
-<!-- Minimizer horizontal -->
 <div class="flex h-screen w-screen flex-col items-center px-6">
-	<div class="justify-left mt-1 flex w-full translate-x-[-22px]">
+	<div class="justify-left mt-2 flex w-full translate-x-[-18px]">
 		<Minimizer onclick={() => goto('/planner')} direction="left" />
 	</div>
-	<PhotoFrame />
+	<div class="mt-2 mb-1">
+		<PhotoFrame />
+	</div>
 	<ItemName />
 	<IngredientInput />
-	<NutrientSum kcal={0} protein={0} />
-	<!-- Create Item -->
+	<div class="mt-3 w-full">
+		<NutrientSum kcal={0} protein={0} />
+	</div>
+	<div class="my-6">
+		<Button text="Create Item" onclick={() => {}} disabled={true} />
+	</div>
 </div>
