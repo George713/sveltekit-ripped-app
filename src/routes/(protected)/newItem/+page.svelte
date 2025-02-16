9e893101634b46b41e74f72720bdcc7794eece05
Friedrich G. Froebel <froebel.business@gmail.com>
2025-02-16 13:28:06 +0100

    🚧Add Minimizer to newItem

---
+++ src/routes/(protected)/newItem/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import Minimizer from '$lib/components/newDesign/atoms/Minimizer.svelte';
+</script>
+
+<!-- Minimizer horizontal -->
+<div class="flex h-screen w-screen flex-col">
+	<Minimizer onclick={() => goto('/planner')} direction="left" />
+</div>
+<!-- Photo Frame -->
+<!-- Item Name -->
+<!-- Add & Record Button (grow) -->
+<!-- kcal | protein sums -->
+<!-- Create Item -->
