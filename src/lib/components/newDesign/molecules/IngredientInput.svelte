167ae56b2f1060266946400f9ebd8cdf5a136b10
Friedrich G. Froebel <froebel.business@gmail.com>
2025-02-18 11:49:22 +0100

    🍱Add audio recording and transcription

--- src/lib/components/newDesign/molecules/IngredientInput.svelte
+++ src/lib/components/newDesign/molecules/IngredientInput.svelte
@@ -1,14 +1,26 @@
 <script lang="ts">
+	import { audioRecorder } from '$lib/audioRecorder.svelte';
 	import Button from '../atoms/Button.svelte';
-
-	let toggle = $state(false);
-
-	const toggleRecording = () => {
-		toggle = !toggle;
-	};
+	import FastSpinner from '../icons/FastSpinner.svelte';
 </script>
 
 <div class="flex w-full flex-1 flex-col justify-end">
+	<!-- Ingredient Field -->
+	<div class="mx-1 mb-2">
+		{#if audioRecorder.isProcessing}
+			<div class="flex justify-center">
+				<FastSpinner />
+			</div>
+		{:else}
+			<p class="text-center text-sm text-stone-200">
+				{#if audioRecorder.isListening}
+					Listening...
+				{:else}
+					{(audioRecorder.recordedText.trim() + ' ' + audioRecorder.tempTranscript.trim()).trim()}
+				{/if}
+			</p>
+		{/if}
+	</div>
 	<!-- Buttons -->
 	<div class="flex space-x-1">
 		<Button text="Add" variant="secondary" icon="plus" classAddons="grow" />
@@ -17,8 +29,8 @@
 			variant="secondary"
 			icon="microphone"
 			classAddons="grow"
-			{toggle}
-			onclick={toggleRecording}
+			toggle={audioRecorder.isRecording}
+			onclick={audioRecorder.record}
 		/>
 	</div>
 </div>
