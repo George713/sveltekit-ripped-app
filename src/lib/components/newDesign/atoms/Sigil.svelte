f32fea7b6886eb9b299468103ef8e57ec8719d0e
Friedrich G. Froebel <froebel.business@gmail.com>
2025-03-06 16:33:08 +0100

    💄Add small style option to sigil component

--- src/lib/components/newDesign/atoms/Sigil.svelte
+++ src/lib/components/newDesign/atoms/Sigil.svelte
@@ -1,16 +1,18 @@
 <script lang="ts">
 	interface Props {
 		rank: 'tbd' | 'wood' | 'bronze' | 'silver' | 'gold' | 'platinum';
+		size?: 'small' | 'large';
 	}
 
-	let { rank }: Props = $props();
+	let { rank, size = 'large' }: Props = $props();
 </script>
 
-<div class="flex w-full justify-center">
+<div class={{ 'flex w-full justify-center': size === 'large' }}>
 	<img
 		class={{
-			'w-[320px]': true,
-			'opacity-50 blur-[2px]': rank === 'tbd'
+			'w-[320px]': size == 'large',
+			'opacity-50 blur-[2px]': rank === 'tbd' && size === 'large',
+			'h-[51px] w-[82px] opacity-50 blur-[1.5px]': size === 'small'
 		}}
 		src="/sigils/{rank}.webp"
 		alt="Sigil Crest"
