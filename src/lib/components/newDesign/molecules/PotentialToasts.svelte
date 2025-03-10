95f22d4d922f6a116ad155126d3bb857a7385b59
Friedrich G. Froebel <froebel.business@gmail.com>
2025-03-10 11:32:38 +0100

    💄Make toasts be above even fireworks

--- src/lib/components/newDesign/molecules/PotentialToasts.svelte
+++ src/lib/components/newDesign/molecules/PotentialToasts.svelte
@@ -4,7 +4,7 @@
 	import Toast from '../atoms/Toast.svelte';
 </script>
 
-<div class="absolute z-10 mt-5 flex w-full flex-col justify-center space-y-2 px-5">
+<div class="absolute z-60 mt-5 flex w-full flex-col justify-center space-y-2 px-5">
 	{#each toastManager.toasts as toast}
 		<Toast {toast} />
 	{/each}
