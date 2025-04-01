cae9fca817db0396d10512dcbc9dbab7db7a113a
Friedrich G. Froebel <froebel.business@gmail.com>
2025-04-01 17:29:32 +0200

    🔥Remove unused code

--- src/lib/components/newDesign/organisms/ActionPanel.svelte
+++ src/lib/components/newDesign/organisms/ActionPanel.svelte
@@ -5,9 +5,6 @@
 	import ActionButtons from '../molecules/ActionButtons.svelte';
 	import AdditionalActionButtons from '../molecules/AdditionalActionButtons.svelte';
 	import TrackingWidget from '../molecules/TrackingWidget.svelte';
-
-	let xpGained = $state<number | undefined>(undefined);
-	let briefGlow = $state(false);
 </script>
 
 <div class="mt-auto flex flex-col items-center">
