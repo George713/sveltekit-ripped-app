10a87fccd8790027ef86d9f1e57f8c337d3ff045
Friedrich G. Froebel <froebel.business@gmail.com>
2025-04-07 13:52:09 +0200

    🚚Rename newItem to upsertItem

--- src/lib/components/newDesign/molecules/CardArray.svelte
+++ src/lib/components/newDesign/molecules/CardArray.svelte
@@ -35,7 +35,7 @@
 	let selectionManagerType = $derived(selectionManager?.classname);
 
 	const pressHandler = new PressHandler({
-		longPress: (id: string | number) => goto(`/newItem?origin=${currentPath}&foodId=${id}`),
+		longPress: (id: string | number) => goto(`/upsertItem?origin=${currentPath}&foodId=${id}`),
 		pressDuration: 1000
 	});
 </script>
@@ -53,7 +53,9 @@
 			{theme}
 			type="newElement"
 			onclick={() =>
-				goto(itemManagerType === 'FoodSetManager' ? '/newSet' : `/newItem?origin=${currentPath}`)}
+				goto(
+					itemManagerType === 'FoodSetManager' ? '/newSet' : `/upsertItem?origin=${currentPath}`
+				)}
 		/>
 	{/if}
 	{#if itemManagerType === 'FoodItemManager'}
