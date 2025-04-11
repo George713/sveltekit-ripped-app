3e88edf55d736d78995a9787f6ba6515eacdf267
Friedrich G. Froebel <froebel.business@gmail.com>
2025-04-11 11:23:13 +0200

    💄Add maximum size to logo

--- src/lib/components/newDesign/atoms/BGLogoBig.svelte
+++ src/lib/components/newDesign/atoms/BGLogoBig.svelte
@@ -1,1 +1,3 @@
-<img class="scale-[calc(85%)] opacity-30" src="/pwa-512x512.png" alt="bg-logo" />
+<div class="flex w-full justify-center">
+	<img class="max-w-[500px] scale-[calc(85%)] opacity-30" src="/pwa-512x512.png" alt="bg-logo" />
+</div>
