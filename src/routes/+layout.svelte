12fdc8f66daa2030e19335daa61fd8969f63e9ee
Friedrich G. Froebel <froebel.business@gmail.com>
2025-04-13 17:47:00 +0200

    🧑‍💻Hide install prompt window for dev

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import '../app.css';
 	import { page } from '$app/stores';
 	import PwaInstallPrompt from '$lib/components/newDesign/overlays/PwaInstallPrompt.svelte';
+	import { PUBLIC_ONMOBILE } from '$env/static/public';
 
 	let { children } = $props();
 
@@ -12,7 +13,7 @@
 <div class="flex h-screen w-screen flex-col bg-linear-to-br/oklch from-stone-900 to-stone-800">
 	{@render children?.()}
 
-	{#if isRootPath}
+	{#if isRootPath && PUBLIC_ONMOBILE === 'TRUE'}
 		<PwaInstallPrompt />
 	{/if}
 </div>
