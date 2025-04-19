4f53cb5c458f1e524412f67dbcb453a254a5c8c3
Friedrich G. Froebel <froebel.business@gmail.com>
2025-04-19 09:57:58 +0200

    🍱Implement array component group in /log

--- src/routes/(protected)/log/+page.svelte
+++ src/routes/(protected)/log/+page.svelte
@@ -1,37 +1,47 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
-	import CardArrayBackground from '$lib/components/newDesign/atoms/CardArrayBackground.svelte';
-	import DigitalCounter from '$lib/components/newDesign/atoms/DigitalCounter.svelte';
+
+	import { calorieManager, plannedItemManager, proteinManager } from '$lib/stateManagers.svelte';
+
+	import { Array } from '$lib/components/newDesign/atoms/array';
 	import Minimizer from '$lib/components/newDesign/atoms/Minimizer.svelte';
-	import SelectionHeader from '$lib/components/newDesign/atoms/SelectionHeader.svelte';
 	import AddButtons from '$lib/components/newDesign/molecules/AddButtons.svelte';
 	import AddOverlay from '$lib/components/newDesign/molecules/AddOverlay.svelte';
-	import CardArray from '$lib/components/newDesign/molecules/CardArray.svelte';
-	import { calorieManager, plannedItemManager, proteinManager } from '$lib/stateManagers.svelte';
+	import DigitalCounter from '$lib/components/newDesign/atoms/DigitalCounter.svelte';
+	import SelectionHeader from '$lib/components/newDesign/atoms/SelectionHeader.svelte';
 
 	let showAddOverlay = $state(false);
 </script>
 
-<div class="flex h-full w-full flex-col items-center">
-	<div class="mb-1 flex w-full grow flex-col overflow-hidden p-2">
-		<div class="flex w-full">
-			<SelectionHeader icon="cutlery" text="Daily Log" />
-		</div>
-		<CardArrayBackground color="dark" classAddons="pt-2 pb-4 flex-col">
-			<CardArray itemManager={plannedItemManager} theme="dark" verticalScroll={true} />
-			<AddButtons bind:showAddOverlay />
-		</CardArrayBackground>
-	</div>
+<!-- Page -->
+<div
+	class={{
+		'flex h-full w-full flex-col items-center': true
+	}}
+>
+	<!-- Daily Log -->
+	<Array.Wrapper>
+		<SelectionHeader icon="cutlery" text="Daily Log" />
+		<Array.Background color="dark">
+			<Array.ArrayComponent itemManager={plannedItemManager} theme="dark" />
+			<div class="pt-3">
+				<AddButtons bind:showAddOverlay />
+			</div>
+		</Array.Background>
+	</Array.Wrapper>
 	<DigitalCounter
 		kcalCurrent={calorieManager.eaten}
 		kcalTarget={calorieManager.target}
 		proteinCurrent={proteinManager.eaten}
 		proteinTarget={proteinManager.target}
+		classAddons="mt-1 mb-4"
 	/>
-	<div class="mt-3 mb-1 flex w-full justify-center">
+
+	<div class="">
 		<Minimizer onclick={() => goto('/')} direction="down" />
 	</div>
 </div>
+
 {#if showAddOverlay}
 	<AddOverlay bind:showAddOverlay />
 {/if}
