285bc8357a14d8d5185fefc3d6f80086b1a8e78b
Friedrich G. Froebel <froebel.business@gmail.com>
2025-05-17 15:01:49 +0200

    🐛Fix initialisation of audioRecorder to cover correct lang

--- src/lib/components/molecules/IngredientInput.svelte
+++ src/lib/components/molecules/IngredientInput.svelte
@@ -7,6 +7,7 @@
 	import Ingredient from '$atoms/Ingredient.svelte';
 	// Icons
 	import FastSpinner from '$icons/FastSpinner.svelte';
+	import { onMount } from 'svelte';
 
 	interface Props {
 		hidden3rdBtn?: boolean;
@@ -14,7 +15,11 @@
 
 	const { hidden3rdBtn = false }: Props = $props();
 
-	const audioRecorder = new AudioRecorder();
+	let audioRecorder = $state<AudioRecorder>();
+
+	onMount(() => {
+		audioRecorder = new AudioRecorder();
+	});
 </script>
 
 <div class="flex w-full flex-1 flex-col justify-end">
@@ -32,16 +37,16 @@
 				/>
 			{/each}
 		</div>
-		{#if audioRecorder.isProcessing}
+		{#if audioRecorder!.isProcessing}
 			<div class="mt-1 flex h-9 items-center justify-center">
 				<FastSpinner />
 			</div>
-		{:else if audioRecorder.isRecording}
+		{:else if audioRecorder!.isRecording}
 			<p class="mt-1 flex min-h-9 items-center justify-center text-center text-sm text-stone-200">
-				{#if audioRecorder.isListening}
+				{#if audioRecorder!.isListening}
 					Listening...
 				{:else}
-					{[audioRecorder.recordedText, audioRecorder.tempTranscript]
+					{[audioRecorder!.recordedText, audioRecorder!.tempTranscript]
 						.map((text) => text.trim())
 						.filter((text) => text) // filters empty strings
 						.join(', ')}
@@ -77,8 +82,8 @@
 			type="button"
 			icon="microphone"
 			classAddons={hidden3rdBtn ? 'basis-1/3' : 'grow'}
-			toggle={audioRecorder.isRecording}
-			onclick={audioRecorder.record}
+			toggle={audioRecorder!.isRecording}
+			onclick={audioRecorder!.record}
 		/>
 	</div>
 </div>
