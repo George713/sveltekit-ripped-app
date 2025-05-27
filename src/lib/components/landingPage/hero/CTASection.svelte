096c59c3c5968a5af0b944636c8f5e6d979f711d
Friedrich G. Froebel <froebel.business@gmail.com>
2025-05-27 14:26:46 +0200

    🚸Add excitement to button after successfully joining waitlist

--- src/lib/components/landingPage/hero/CTASection.svelte
+++ src/lib/components/landingPage/hero/CTASection.svelte
@@ -82,7 +82,7 @@
 				onclick={() => (successModalOpen = false)}
 				class="w-full rounded-md bg-indigo-600 px-4 py-2.5 font-medium text-white hover:bg-indigo-700 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-stone-800 focus:outline-none"
 			>
-				Okay
+				See you on June 2nd!
 			</button>
 		</div>
 	</div>
